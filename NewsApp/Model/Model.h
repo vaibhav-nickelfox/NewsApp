eac7c39d9cd7396605104d94637f2e0b44e59f12
Vaibhav Parmar <vaibhav@nickelfox.com>
2017-11-09 09:23:34 +0530

    feat: setup Model

---
+++ NewsApp/Model/Model.h
@@ -0,0 +1,19 @@
+//
+//  Model.h
+//  Model
+//
+//  Created by Vaibhav Parmar on 09/11/17.
+//  Copyright © 2017 Vaibhav Parmar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Model.
+FOUNDATION_EXPORT double ModelVersionNumber;
+
+//! Project version string for Model.
+FOUNDATION_EXPORT const unsigned char ModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Model/PublicHeader.h>
+
+
