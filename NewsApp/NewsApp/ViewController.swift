af5ca2c36f43e429d4b8c0263ff225455c2824d1
Vaibhav Parmar <vaibhav@nickelfox.com>
2017-11-09 08:48:23 +0530

    feat: setup initial project

---
+++ NewsApp/NewsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Vaibhav Parmar on 09/11/17.
+//  Copyright © 2017 Vaibhav Parmar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
